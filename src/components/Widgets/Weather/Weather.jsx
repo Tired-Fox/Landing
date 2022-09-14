9ae90cf98f4913b82cf0064f0ab6a1227ae96035
Zachary Boehm <zboehm@menard-inc.com>
2022-09-14 13:32:08 -0500

    finished weather widget

--- src/components/Widgets/Weather/Weather.jsx
+++ src/components/Widgets/Weather/Weather.jsx
@@ -1,6 +1,12 @@
 import { h, Fragment } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
-import { GetWeather, GetAirPollution, GetForecast, parseWeather, parseForecast } from './_Weather';
+import {
+    GetWeather,
+    GetAirPollution,
+    GetForecast,
+    parseWeather,
+    parseForecast
+} from './_Weather';
 import './weather.scss';
 
 const unitSymbols = {
@@ -10,6 +16,72 @@ const unitSymbols = {
     metric: {
         temp: "℃"
     }
+}
+
+export function HourlyCard({ ...props }) {
+
+    return (
+        <>
+            <div class="forecast-hourly">
+                <div class="time">
+                    <p class="h6">{props.data.dateTime.toFormat('t')}</p>
+                </div>
+                <img src={props.data.icon} alt="Forecast icon" />
+                <div class="data">
+                    <h3
+                        class="temp"
+                        title={`H: ${props.data.temp.max} | L: ${props.data.temp.min}`}
+                    >
+                        {props.data.temp.actual}
+                    </h3>
+                    <div class="extra">
+                        <p class="h6" title={`Humidity: ${props.data.temp.hum}`}>{props.data.temp.hum}% <i class="fa-solid fa-droplet"></i> {Math.floor(props.data.cor * 100)}%</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export function WeatherHourlyCards({ ...props }) {
+    const forecast = props.forecast;
+
+    return (
+        <>
+            <div class="forecast-day">
+
+                <h3>{forecast[0].date}</h3>
+                <div>
+                    {
+                        forecast.map((hour) => {
+                            return (
+                                <HourlyCard data={hour} />
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
+
+export function Forecast({ ...props }) {
+    let forecast = [];
+    for (const day in props.forecast) {
+        forecast.push(props.forecast[day]);
+    }
+
+    return (
+        <>
+            {
+                forecast.map((day) => {
+                    return (
+                        <WeatherHourlyCards forecast={day} />
+                    )
+                })
+            }
+        </>
+    );
 }
 
 // Uses OpenWeatherMap
@@ -36,7 +108,7 @@ export function WeatherWidget({ ...props }) {
             <div>
                 {
                     weather &&
-                    <Weather weather={weather} />
+                    <Weather weather={weather} forecast={forecast} />
                 }
             </div>
         </>
@@ -51,26 +123,36 @@ export function Weather({ ...props }) {
             <div class="weather-widget">
 
                 <h3>{weather.date}</h3>
-                <div class="weather-overall">
-                    <div class="weather-desc">
-                        <img src={weather.icon} alt="Weather.icon" />
-                        <p>{weather.description}</p>
+                <a href="#" onClick={(e) => {
+                    e.preventDefault();
+                    document.getElementById("forecast").classList.toggle('collapse');
+                }}>
+                    <div class="weather-overall">
+                        <div class="weather-desc">
+                            <img src={weather.icon} alt="Weather.icon" />
+                            {/* <p>{weather.description}</p> */}
+                        </div>
+                        <div class="weather-temp">
+                            <h1 title={`H: ${weather.temp.max + unitSymbols[weather.unit].temp}  L: ${weather.temp.min + unitSymbols[weather.unit].temp}`}>
+                                {weather.temp.actual}<sup class="h4 poppins">{unitSymbols[weather.unit].temp}</sup>
+                            </h1>
+                            <p class="mobile h5">H: {weather.temp.max} | L: {weather.temp.min}</p>
+                        </div>
+                        <div class="weather-additional">
+                            <p title="Wind speed and direction"><strong><i class="fa-solid fa-wind"></i> {weather.wind.speed} mph {weather.wind.dir}</strong></p>
+                            <p title="Air pollution"><strong><i class="fa-solid fa-virus"></i> {weather.aqi}</strong></p>
+                            <p title="Feels like"><strong><i class="fa-solid fa-temperature-three-quarters"></i> {weather.temp.feels}<sup>{unitSymbols[weather.unit].temp}</sup></strong></p>
+                        </div>
                     </div>
-                    <div class="weather-temp">
-                        <h1 title={`H: ${weather.temp.max + unitSymbols[weather.unit].temp}  L: ${weather.temp.min + unitSymbols[weather.unit].temp}`}>
-                            {weather.temp.actual}<sup class="h4 poppins">{unitSymbols[weather.unit].temp}</sup>
-                        </h1>
-                    </div>
-                    <div class="weather-additional">
-                        <p title="Wind speed and direction"><strong><i class="fa-solid fa-wind"></i> {weather.wind.speed} mph {weather.wind.dir}</strong></p>
-                        <p title="Air pollution"><strong><i class="fa-solid fa-virus"></i> {weather.aqi}</strong></p>
-                        <p title="Feels like"><strong><i class="fa-solid fa-temperature-three-quarters"></i> {weather.temp.feels}<sup>{unitSymbols[weather.unit].temp}</sup></strong></p>
-                    </div>
+                </a>
+
+                <div id="forecast" class="collapse">
+                    {
+                        props.forecast &&
+                        <Forecast forecast={props.forecast} className="mt-5" />
+                    }
                 </div>
-                {/* <div class="hr"></div>
-                <div class="forecast">
-                    <h3>Forecast</h3>
-                </div> */}
+
             </div>
         </>
     );
